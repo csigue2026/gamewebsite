f341797cc653aaa45a5bc44782c3a3a4d7f4dcb1
csigue2026 <116986093+csigue2026@users.noreply.github.com>
2023-03-23 17:17:43 +0800

    Update gamenameJS.js

--- gamenameJS.js
+++ gamenameJS.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('grid')
-    let squares = Array.from(document.querySelectorAll('grid div'))
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
     const ScoreDisplay = document.querySelector('#score')
-    const playbtn = document.querySelector('#start-button')
-    const width = 15
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
 
     const STetromino = [
-        [1, width+1, width*2+1, 2],
-        [1, width+1, width+2, width*2+1],
-        [width, width+1, width+2, width*2+1],
-        [1, width, width+1, width*2+1]
+        [1, width+1, width+2+1, 2],
+        [1, width+1, width+2, width+2+1],
+        [width, width+1, width+2, width+2+1],
+        [1, width, width+1, width+2+1]
     ]
 
     const zTetromino = [
-        [0, width, width+1, width*2+1],
+        [0, width, width+1, width+2+1],
         [width+1, width+2, width*2, width*2+1],
         [0, width, width+1, width*2+1],
         [width+1, width+2, width*1, width*2+1]
@@ -21,9 +21,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const tTetromino = [
         [1, width, width+1, width+2],
-        [width, width+1, width+2, width*2+1],
-        [1, width+1, width*2+1, width+2],
-        [width, width+2, width*2+1, width*2+2]
+        [width, width+1, width+2, width+2+1],
+        [1, width+1, width+2+1, width+2],
+        [width, width+2, width+2+1, width+2+2]
     ] 
 
     const oTetromino = [
@@ -34,12 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
     ] 
 
     const iTetromino = [
-        [1, width+1, width* 2+1, width*3+1],
+        [1, width+1, width+2+1, width+3+1],
         [width, width+1, width+2, width+3],
-        [1, width+1, width*2+1, width*3+1],
-        [width, width+1, width+2, width+3]
-    ]
-    
+        [1, width+1, width+2+1, width+3+1],
+        [width, width+1, width+2, width+3],
+    ] 
     const xTetromino = [
         [1, width, width+1, width+2,width*2+1],
         [1, width, width+1, width+2,width*2+1],
@@ -89,13 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
         [0, 1, width+1, width*2+1],
         [2, width, width+1, width+2]
     ]
-    const theTetrominoes = [STetromino, zTetromino, tTetromino, oTetromino, iTetromino, xTetromino, yTetromino, vTetromino, uTetromino, lTetromino, TTetromino, fTetromino, wTetromino]
+    const theTetrominoes = [STetromino, zTetromino, tTetromino, oTetromino, iTetromino, xTetromino, yTetromino, wTetromino, vTetromino, uTetromino, lTetromino, TTetromino, fTetromino]
 
     let currentPosition = 4
-    let currentRotation = 0
-
-    let random = Math.floor(Math.random()*theTetrominoes.length)
-    let current = theTetrominoes[random][currentRotation]
+    let current = theTetrominoes[0][0]
 
     function control(a){
         if(a.keyCode === 65){
@@ -108,66 +104,40 @@ document.addEventListener('DOMContentLoaded', () => {
             Gooo()
         }
         else if(a.keyCode === 83){
-            iAmSpeed()
+            moveDown()
         }
     }
-var START = 0;
-function startGameButton(){
-    START++;
-}
-function pauseEnd(){
-    START--;
-}
-if(START == 1){
+
     function draw() {
         current.forEach( index =>  {
             squares[currentPosition + index].classList.add('tetromino')
         })
     }
-    function Undraw() {
+
+    function undraw() {
         current.forEach( index =>  {
             squares[currentPosition + index].classList.remove('tetromino')
         })
     }
-    var difficultyLevel = 1
-    var mode = 0
-    function difficulty(){
-        if(difficultyLevel == 1){
-            timerId = setInterval(iAmSpeed, 1000)}
-            else if(difficultyLevel == 2){
-                timerId = setInterval(iAmSpeed, 420)}
-            else if(difficultyLevel == 3){
-                    timerId = setInterval(iAmSpeed, 10)}
-            else if(difficultyLevel == 4){
-                timerId = setInterval(iAmSpeed, 2000)}
-        if(mode == 0){
-            difficultyLevel++;
-            mode++;
-        }
-        else if(mode == 1){
-            difficultyLevel++;
-            mode++;
-        }
-        else if(mode == 2){
-            difficultyLevel++;
-            mode++;
-        }
-        else if(mode == 3){
-            difficultyLevel--;
-            difficultyLevel--;
-            difficultyLevel--;
-            mode--;
-            mode--;
-            mode--;
+
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('tetromino'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('tetromino'));
+
+            random = Math.floor(Math.random() * theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw()
         }
 
     }
+    timerId = setInterval(moveDown, 1000)
     
-
-    function iAmSpeed(){
-        Undraw() 
-        currentPosition += width;
+    function moveDown() {
+        undraw()
+        currentPosition += width
         draw()
+        freeze()
     }
     function Unfall(){
         if(current.some(index => squares[currentPosition + index + width].classList.contains('tite'))){
@@ -212,8 +182,9 @@ if(START == 1){
         current = theTetrominoes[random][currentRotation]
         draw()
     }
-    const dS = Document.querySelectorAll('.mini-grid-div')
+    const dS = document.querySelectorAll('.mini-grid-div')
     const dw = 4
     let dI = 0
-}
-})
+    
+    draw()
+}) 
